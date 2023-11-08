f388a4bb566905485f489e7a99fc7adbf318d68f
psi7218 <133967948+psi7218@users.noreply.github.com>
2023-11-08 16:53:06 +0900

    Title: 점수대 파악하기, Time: 118ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231108/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[]{0,0,0,0,0,0,0,0,0,0};
+        
+        while (true) {
+        	int val = Integer.parseInt(st.nextToken());
+        	if (val == 0) {
+        		break;
+        	} else if (val > 9){
+        		val = val/10;
+        		
+        		arr[val-1] += 1;
+        		
+        	}
+        }
+        for (int i=9; i>-1; i--) {
+        	System.out.println((i+1)*10 + " - " + arr[i]);
+        }
+    }
+}
