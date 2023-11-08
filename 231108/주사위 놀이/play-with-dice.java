24c076027bfc08acdec5cb1fb73348eb6a10f612
psi7218 <133967948+psi7218@users.noreply.github.com>
2023-11-08 16:34:07 +0900

    Title: 주사위 놀이, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 231108/주사위 놀이/play-with-dice.java
+++ 231108/주사위 놀이/play-with-dice.java
@@ -1,5 +1,24 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+
+    public static void main(String[] args) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[]{0,0,0,0,0,0};
+        
+        for (int i=0; i<10; i++) {
+        	int val = Integer.parseInt(st.nextToken());
+        	
+        	arr[val-1] += 1;
+        	
+        }
+        for (int i=0; i<6; i++) {
+        	System.out.println(i+1 + " - " + arr[i]);
+        }
     }
 }
