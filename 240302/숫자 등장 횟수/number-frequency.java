41c612bf1f1c208fbc666654b5c2ffa8d3d75b3b
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-03-02 14:08:01 +0900

    Title: 숫자 등장 횟수, Time: 864ms, Memory: 35MB, Status: Wrong Answer - CodeTree

---
+++ 240302/숫자 등장 횟수/number-frequency.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[100001];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            int currentValue = hashMap.getOrDefault(key,0);
+            hashMap.put(key, currentValue+1);
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int question = Integer.parseInt(st.nextToken());
+            if (0 == hashMap.getOrDefault(question,0)) {
+                System.out.println(0 + " ");
+            } else {
+                System.out.print(hashMap.get(question) + " ");
+            }
+        }
+
+
+
+
+    }
+}
