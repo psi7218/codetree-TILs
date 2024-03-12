bc8606ee2e35f6c3d31c38629abc09730a90bc85
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-03-12 22:32:39 +0900

    Title: 삼 오 무, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240312/삼 오 무/three-five-moo.java
+++ 240312/삼 오 무/three-five-moo.java
@@ -15,16 +15,16 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
 
         int left = 1;
-        int right = 1000000001;
+        int right = 1000000000;
         int answer = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            int val = mid - mid/3 - mid / 5 ;
+            int val = mid/ 3 + mid /5 - mid /15;
 
-            if (val == n) {
+            if (mid - val == n) {
                 answer = mid;
                 break;
-            } else if (val < n) {
+            } else if (mid -val < n) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
