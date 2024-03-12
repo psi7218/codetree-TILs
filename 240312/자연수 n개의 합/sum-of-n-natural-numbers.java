728155f1980229add194d52ef43472e4ef6935a1
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-03-12 21:12:54 +0900

    Title: 자연수 n개의 합, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240312/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 240312/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -11,20 +11,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Long n = Long.parseLong(br.readLine());
+        Long s = Long.parseLong(br.readLine());
 
         long left = 1;
-        long right = n;
-        long answer = n;
+        long right = s;
+        long answer = 0;
         while (left <= right) {
             long mid = (left + right) / 2;
             long val = mid * (mid + 1) / 2;
-            if (val > n) {
-                answer = Math.min(mid,answer) - 1;
-                right = mid - 1;
-            } else {
-
+            if (val <= s) {
+                answer = mid;
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
 
         }
