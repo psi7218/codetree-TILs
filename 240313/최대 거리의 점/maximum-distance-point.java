74327fab236edec2435837eb35148d1933454c4f
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-03-13 13:01:04 +0900

    Title: 최대 거리의 점, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240313/최대 거리의 점/maximum-distance-point.java
+++ 240313/최대 거리의 점/maximum-distance-point.java
@@ -9,8 +9,9 @@ public class Main {
     static int[] arr;
     public static void main(String [] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
         for (int i = 0; i < n; i++) {
