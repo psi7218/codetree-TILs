bc5975d9a33891012ca9aa6c65a4f6eb18d2695f
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-03-13 13:00:09 +0900

    Title: 최대 거리의 점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240313/최대 거리의 점/maximum-distance-point.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr,0,n);
+
+        int left = 0;
+        int right = 1000000000;
+        int answer = 0;
+
+        while ( left <= right ) {
+            int mid = (left + right) / 2;
+            if ( func(mid) ) {
+                left = mid + 1;
+                answer = Math.max(answer, mid);
+            }
+            else {
+                right = mid - 1;
+            }
+
+        }
+
+
+    }
+    static boolean func(int dist) {
+        int cnt = 1;
+        int currentIdx = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - arr[currentIdx] >= dist) {
+                cnt ++;
+                currentIdx = i;
+            }
+        }
+        return cnt >= m;
+    }
+}
