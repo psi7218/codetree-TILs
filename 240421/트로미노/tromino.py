ebce2c1e997b5cb189629c86fcb3d9033261cd0a
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-04-21 20:22:53 +0900

    Title: 트로미노, Time: 190ms, Memory: 26MB, Status: Passed - CodeTree

--- 240421/트로미노/tromino.py
+++ 240421/트로미노/tromino.py
@@ -1,34 +1,64 @@
-n, m = map(int, input().split())
+# 변수 선언 및 입력:
+
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 가능한 모든 모양을 전부 적어줍니다.
+shapes = [
+    [[1, 1, 0],
+    [1, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0]],
+]
+
+# 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+def get_max_sum(x, y):
+    max_sum = 0
+    for i in range(6):
+        is_possible = True
+        sum_of_nums = 0
+        for dx in range(0, 3):
+            for dy in range(0, 3):
+                if shapes[i][dx][dy] == 0:
+                    continue
+                if x + dx >= n or y + dy >= m:
+                    is_possible = False
+                else:
+                    sum_of_nums += grid[x + dx][y + dy]
+        
+        if is_possible:
+            max_sum = max(max_sum, sum_of_nums)
+
+    return max_sum
 
 
-di = [0,1,0,-1]
-dj = [1,0,-1,0]
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+ans = 0
 
-answer = 0
-
-
-def func(x, y, sum1, depth, visited):
-    global answer
-
-    if depth == 3:
-        answer = max(answer, sum1)
-
-        return
-
-    for k in range(4):
-        ni = x + di[k]
-        nj = y + dj[k]
-
-        if 0 <= ni < n and 0 <= nj < m:
-            if [ni,nj] not in visited:
-                func(ni, nj, sum1 + arr[ni][nj], depth + 1, visited + [ni,nj])
-
-
+# 격자의 각 위치에 대하여 탐색하여줍니다.
 for i in range(n):
     for j in range(m):
-        func(i, j, arr[i][j], 1, [[i,j]])
+        ans = max(ans, get_max_sum(i, j))
 
-print(answer)
+print(ans)
