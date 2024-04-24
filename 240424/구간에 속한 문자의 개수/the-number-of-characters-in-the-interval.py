c1182da0d45b361b9209f56934caf531d6a7142e
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-04-24 09:19:22 +0900

    Title: 구간에 속한 문자의 개수, Time: 5023ms, Memory: 45MB, Status: Time Limit Exceed - CodeTree

--- 240424/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 240424/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -5,13 +5,13 @@ for _ in range(n):
 
 
 for _ in range(k):
-    i, j, k, l = map(int, input().split())
+    i, j, m, n = map(int, input().split())
 
     a = 0
     b = 0
     c = 0
-    for x in range(i-1, k):
-        for y in range(j-1, l):
+    for x in range(i-1, m):
+        for y in range(j-1, n):
             if arr[x][y] == 'a':
                 a += 1
             elif arr[x][y] == 'b':
