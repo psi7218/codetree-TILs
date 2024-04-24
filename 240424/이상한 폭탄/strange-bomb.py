6570bf6a4bd843c834e54bf433b18e0872509ac1
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-04-24 21:33:20 +0900

    Title: 이상한 폭탄, Time: 175ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240424/이상한 폭탄/strange-bomb.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+n, k = map(int, input().split())
+
+check = dict()
+
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+answer = 0
+for idx in range(n):
+    if arr[idx] in check:
+        if idx - check[arr[idx]] <= k:
+            answer = max(answer, arr[idx])
+    else:
+        check[arr[idx]] = idx
+
+
+print(answer)
