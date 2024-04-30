d7ee09aadd487637a1b871aa3133ea403cfac4d3
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-01 02:15:20 +0900

    Title: 좌우 반전시키기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+answer = 0
+for i in range(1,n - 1):
+	if arr[i-1] == 0:
+		arr[i-1] = 1
+		if arr[i] == 1:
+			arr[i] = 0
+		else:
+			arr[i] = 1
+		if arr[i+1] == 1:
+			arr[i+1] = 0
+		else:
+			arr[i+1] = 1
+
+		answer += 1
+
+if arr[n-1] == 0:
+	print(-1)
+else:
+	print(answer)
