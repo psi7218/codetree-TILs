a8a2ac42f1c867535fce54ca9d7584fa569365f4
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-01 02:20:32 +0900

    Title: 좌우 반전시키기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/좌우 반전시키기/flip-left-and-right.py
+++ 240501/좌우 반전시키기/flip-left-and-right.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 answer = 0
-for i in range(1,n - 1):
+for i in range(1,n-1 ):
 	if arr[i-1] == 0:
 		arr[i-1] = 1
 		if arr[i] == 1:
@@ -16,7 +16,7 @@ for i in range(1,n - 1):
 
 		answer += 1
 
-if sum(arr) == n:
+if arr[-1] and arr[-2] and sum(arr[:n-2]) == n - 2:
 	print(answer)
 else:
 	print(-1)
