b337fffcce16f3ba8f55fc4ac3aee1e00de6a26d
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-01 18:17:27 +0900

    Title: G & H 반전시키기 3, Time: 113ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240501/G & H 반전시키기 3/reversing-g-and-h-3.py
@@ -0,0 +1,38 @@
+n = int(input())
+
+first = input()
+second = input()
+
+start = []
+end = []
+
+for i in range(n):
+    if first[i] == 'G':
+        start.append(0)
+    else:
+        start.append(1)
+    
+    if second[i] == 'G':
+        end.append(0)
+    else:
+        end.append(1)
+
+answer = 0
+idx = 0
+while idx < n:
+    j = 0
+    if start[idx] != end[idx]:
+        for num in range(1,4):
+            if idx + num <= n -1 :
+                if start[idx+ num] != end[idx + num]:
+                    j = num
+    else:
+        idx += 1
+        continue
+
+    for x in range(idx, idx + j + 1):
+        start[x] ^= 1
+    answer += 1
+    idx = j + 1
+
+print(answer)
