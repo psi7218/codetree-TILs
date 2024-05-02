23c38123f7a7d364db4a55251729f580d707f2da
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-02 20:51:28 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 1354ms, Memory: 51MB, Status: Passed - CodeTree

--- 240502/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 240502/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,26 +1,46 @@
-n, d = map(int, input().split())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+import sys
+from sortedcontainers import SortedSet
 
-arr.sort(key=lambda x : x[1])
+n, d = tuple(map(int, input().split()))
+point = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-start = 0
-end = 1
-answer = 1e9 
+point = [(0, 0)] + sorted(point)
 
-while end < n:
-    temp_x , temp_y = arr[start][0], arr[start][1]
-    now_x, now_y = arr[end][0], arr[end][1]
-    if now_y - temp_y >= d:
-        answer = min(answer, abs(now_x - temp_x))
-        if end - start >= 2:
-            start += 1
-        else:
-            end += 1
-    else:
-        end += 1
+point_count = SortedSet()
 
-if answer == 1e9:
-    answer = -1
-print(answer)
+
+def get_min():
+    if not point_count:
+        return 0
+    return point_count[0][0]
+
+
+def get_max():
+    if not point_count:
+        return 0
+    return point_count[-1][0]
+
+
+ans = sys.maxsize
+
+
+j = 0
+for i in range(1, n + 1):
+    while j + 1 <= n and get_max() - get_min() < d:
+        point_count.add((point[j + 1][1], point[j + 1][0]))
+        j += 1
+
+    if get_max() - get_min() < d:
+        break
+    ans = min(ans, point[j][0] - point[i][0])
+
+    point_count.remove((point[i][1], point[i][0]))
+
+
+if ans == sys.maxsize:
+    print(-1)
+else:
+    print(ans)
