38efca984db17727a146f03343d7c723f14340ce
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-07 22:03:59 +0900

    Title: 마라톤 중간에 택시타기, Time: 285ms, Memory: 33MB, Status: Passed - CodeTree

--- 240507/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
+++ 240507/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -4,13 +4,14 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 L,R = [0] * n, [0] * n
 
 for i in range(1,n):
-    L[i] = abs(arr[i][0] - arr[0][0]) + abs(arr[i][1] - arr[0][1]) 
+    L[i] = abs(arr[i][0] - arr[i-1][0]) + abs(arr[i][1] - arr[i-1][1]) + L[i-1]
 
 for j in range(n-2,-1,-1):
-    R[j] = abs(arr[j][0] - arr[n-1][0]) + abs(arr[j][1] - arr[n-1][1])
+    R[j] = abs(arr[j][0] - arr[j+1][0]) + abs(arr[j][1] - arr[j+1][1]) + R[j+1]
+
 
 answer = 1e9
 for k in range(1, n-1):
-    answer = min(answer, L[k] + R[k])
+    answer = min(answer, L[k-1] + R[k+1] + abs(arr[k+1][0] - arr[k-1][0]) + abs(arr[k+1][1] - arr[k-1][1]))
 
 print(answer)
