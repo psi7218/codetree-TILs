bc629c5129bd120b4d9d3b48d50ed2064e4c957e
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-07 23:29:55 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = [0,0] + list(map(int, input().split())) + [0,0]
+
+L, R = [0] * (n+4), [0] * (n+4)
+max_val = 0
+for i in range(2, n+4):
+    max_val = max(max_val, arr[i-2])
+    L[i-2] = max_val
+
+
+max_val = 0
+for j in range(n+1, -1,-1):
+      max_val = max(max_val, arr[j + 2])
+      R[j+2] = max_val
+
+answer = 0
+for k in range(2, n+2):
+    answer = max(answer, L[k-2] + R[k+2] + arr[k])
+
+
+print(answer)
