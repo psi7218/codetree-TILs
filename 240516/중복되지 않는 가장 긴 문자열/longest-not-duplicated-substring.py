00bbea26b96775e5e8aa2d61ea2ce0ce6b35493b
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-16 20:41:16 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240516/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
@@ -0,0 +1,17 @@
+word = input()
+n = len(word)
+record = dict()
+
+start = 0
+max_val = 0
+for i in range(n):
+    if word[i] in record:
+        max_val = max(max_val, i - record[word[i]])
+        start = record[word[i]] + 1
+        record[word[i]] = i
+
+    else:
+        record[word[i]] = i
+        max_val = max(max_val, i - start + 1)
+
+print(max_val)
