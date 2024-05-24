fa1eb4afeb6f6dc8ba3f2f5e9e15d2e81b3a85c7
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-05-24 16:07:41 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 280ms, Memory: 73MB, Status: Passed - CodeTree

--- 240524/양쪽 모두 존재하는 구간/both-exist-section.py
+++ 240524/양쪽 모두 존재하는 구간/both-exist-section.py
@@ -5,20 +5,31 @@ num_list = list(map(int, input().split()))
 
 answer = 1e9
 check = defaultdict(int)
+other = defaultdict(int)
+for num in range(1, len(num_list)):
+    other[num_list[num]] += 1
+
 check[num_list[0]] += 1
+
 j = 1
 for i in range(n):
     while j < n and len(check) != m:
         check[num_list[j]] += 1
+        other[num_list[j]] -= 1
+        if other[num_list[j]] == 0:
+            del other[num_list[j]]
         j += 1
 
-    if len(check) == m:
+    if len(check) == m and len(other) == m:
         answer = min(answer, j - i)
 
     if check[num_list[i]] == 1:
         del check[num_list[i]]
+
     else:
         check[num_list[i]] -= 1
+    other[num_list[i]] += 1
+
 if answer == 1e9:
     answer = -1
 
