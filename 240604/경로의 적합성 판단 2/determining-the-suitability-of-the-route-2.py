a0b95e724c74406adbb14f83d34f0c6b70ee4d9f
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-04 15:02:13 +0900

    Title: 경로의 적합성 판단 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240604/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.py
@@ -0,0 +1,24 @@
+n, m, k = map(int, input().split())
+uf= [i for i in range(n+1)]
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x, y):
+    a = find(x)
+    b = find(y)
+
+    uf[a] = b
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    union(x, y)
+
+s, e = map(int, input().split())
+if find(s) == find(e):
+    print(1)
+else:
+    print(0)
