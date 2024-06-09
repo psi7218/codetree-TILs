5b324b44a1862c696a38f3a245add3af9281e768
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-09 17:28:06 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240609/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+uf = [i for i in range(n+1)]
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x, y):
+    a, b = find(x), find(y)
+    uf[b] = a
+
+chart = [[1,i] for i in range(n + 1)]
+graph = [sorted(list(map(int, input().split()))) for _ in range(m)]
+graph.sort(key = lambda x : x[0])
+
+s, t, cnt = map(int, input().split())
+
+phase = [set() for j in range(n+1)]
+
+for start, end in graph:
+    union(start, end)
+    idx = find(start)
+    phase[idx].add(end)
+
+
+for ix in range(len(phase)):
+    chart[ix] = [len(phase[ix]) + 1, ix]
+chart.sort(key = lambda x : -x[0])
+answer = len(phase[s]) + 1
+
+for count, index in chart:
+    if cnt == 0:
+        break
+    if index == s:
+        continue
+
+    if t in phase[index]:
+        continue
+    
+    else:
+        answer += count
+        cnt -= 1
+
+
+print(answer)
