aecafbce802b3eaca07b4419d27fb4106d5bce9a
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-09 14:22:19 +0900

    Title: 정점 연결하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240609/정점 연결하기/connect-vertices.py
@@ -0,0 +1,21 @@
+n = int(input())
+uf = [i for i in range(n+1)]
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x, y):
+    a, b = find(x), find(y)
+    uf[a] = b
+
+for _ in range(n-2):
+    s, e = map(int, input().split())
+    union(min(s, e), max(s, e))
+for i in range(2, n+1):
+    I = find(i)
+    if I != find(1):
+        print(1, I)
+        break
