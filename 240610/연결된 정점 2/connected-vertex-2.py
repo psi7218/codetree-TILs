6d416e67d8e67772f78008d763243613adeb2c77
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-10 14:31:33 +0900

    Title: 연결된 정점 2, Time: 538ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240610/연결된 정점 2/connected-vertex-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+uf = [i for i in range(100000+1)]
+size = [1] * (100000 + 1)
+
+def find(x):
+    if uf[x] == x:
+        return x 
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x, y):
+    a, b = find(x), find(y)
+    if a != b:
+        uf[a] = b 
+        size[b] += size[a]
+
+
+for _ in range(n):
+    s, t = map(int, input().split())
+    union(s, t)
+    print(size[t])
