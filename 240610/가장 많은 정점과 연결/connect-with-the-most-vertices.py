fb85338bc9e68c4611082b08fec8e65dd6ceaccd
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-10 12:31:14 +0900

    Title: 가장 많은 정점과 연결, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240610/가장 많은 정점과 연결/connect-with-the-most-vertices.py
+++ 240610/가장 많은 정점과 연결/connect-with-the-most-vertices.py
@@ -1,6 +1,7 @@
 n, m, k = map(int, input().split())
 points = [0] + list(map(int, input().split()))
 
+
 uf = [i for i in range(n+1)]
 
 def find(x):
@@ -20,16 +21,25 @@ for _ in range(m):
     a, b = map(int, input().split())
     union(a, b)
 
+standard = points[find(1)]
+for index in range(len(points)):
+    points[index] = [points[index], index]
+
+points.sort(key = lambda x : x[0])
+
 answer = 0
 
-for idx in range(2, n+1):
-    standard = find(1)
-    I = find(idx)
+for idx in range(1, len(points)):
+    point = points[idx][0]
+    ix = points[idx][1]
 
-    if standard != I:
-        answer += (points[I] + points[standard])
-        union(1, idx)
-    
+    I = find(ix)
+    if find(1) != I:
+        answer += (standard + point)
+
+        standard = min(standard, point)
+        uf[1] = I
+
 
 if answer <= k:
     print(answer)
