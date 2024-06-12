98ff961e54c9bc209a0bc73bbedcb488e2e5e093
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-12 18:15:12 +0900

    Title: 화성 탐사, Time: 338ms, Memory: 27MB, Status: Passed - CodeTree

--- 240612/화성 탐사/explore-mars.py
+++ 240612/화성 탐사/explore-mars.py
@@ -1,45 +1,76 @@
+from collections import deque
 import heapq
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
+number = [[0] * n for _ in range(n)]
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-INF = int(1e9)
-c, d = 0, 0
-cnt = 0
+mst = []
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x, y):
+    a, b = find(x), find(y)
+    uf[a] = b
+
+count = 1
+num = 1
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 1:
-            c, d = i, j
+        if arr[i][j] == 1 or arr[i][j] == 2:
+            number[i][j] = num
+            num += 1
+
         if arr[i][j] == 2:
-            cnt += 1
-visited = [[INF] * n for _ in range(n)]
-q = []
-heapq.heappush(q, (0, c, d))
-visited[c][d] = 0
+            count += 1
+
+uf = [i for i in range(num+1)]
+
+
+def bfs(c,d):
+    visited = [[-1] * n for _ in range(n)]
+    q = deque()
+    q.append((c, d))
+    visited[c][d] = 0
+
+    while q:
+        x, y = q.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if visited[nx][ny] == -1:
+                    if arr[nx][ny] == 0:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append((nx,ny))
+                    if arr[nx][ny] == 1 or arr[nx][ny] == 2:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append((nx, ny))
+                        mst.append((visited[nx][ny], number[c][d], number[nx][ny]))
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1 or arr[i][j] == 2:
+            bfs(i,j)
+
+mst.sort()
 
 answer = 0
-while q:
-    cost, x, y = heapq.heappop(q)
+for cost, start, end in mst:
+    if find(start) != find(end):
+        union(start, end)
+        answer += cost
+        count -= 1
 
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-
-        if 0 <= nx < n and 0 <= ny < n:
-            if cost + 1 < visited[nx][ny]:
-                if arr[nx][ny] == 0:
-                    heapq.heappush(q, (cost+1, nx, ny))
-                    visited[nx][ny] = cost + 1
-
-                elif arr[nx][ny] == 2 and visited[nx][ny] == INF:
-                    heapq.heappush(q, (0, nx, ny))
-                    answer += cost + 1
-                    visited[nx][ny] = cost + 1
-                    cnt -= 1
-
-if cnt == 0:
+if count == 1:
     print(answer)
 else:
     print(-1)
