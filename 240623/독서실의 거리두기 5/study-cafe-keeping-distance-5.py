ff42fec26f1e9aabc97df3c28539cc546cd47d1f
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-23 12:48:40 +0900

    Title: 독서실의 거리두기 5, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240623/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240623/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -5,12 +5,13 @@ temp = - 1
 for i in range(n):
     if line[i] == 1:
         if temp == -1:
+            answer = i
             temp = i
         else:
             answer = max(answer, (i - temp) // 2)
             temp = i
 
-    if i == n - 1 and line[i] == 0:
+    elif i == n - 1 and line[i] == 0:
         answer = max(answer, (i - temp))
 
 print(answer)
