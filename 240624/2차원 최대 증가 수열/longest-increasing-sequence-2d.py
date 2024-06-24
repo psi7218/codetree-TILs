849d9439e8886e1a5abfb4e87e21a0b98bed2f38
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-24 11:00:25 +0900

    Title: 2차원 최대 증가 수열, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240624/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240624/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -9,16 +9,19 @@ for k in range(1, n):
 
 for l in range(1, m):
     arr[0][l] = int(1e9)
+
 for i in range(1, n):
     for j in range(1, m):
-       for s in range(i):
-           for t in range(j):
-               if arr[s][t] < arr[i][j]:
-                   dp[i][j] = max(dp[i][j], dp[s][t] + 1)
+        for s in range(i):
+            for t in range(j):
+                if arr[s][t] < arr[i][j] and arr[0][0] < arr[i][j]:
+                    dp[i][j] = max(dp[i][j], dp[s][t] + 1)
 
 answer = 1
 for x in range(1, n):
     for y in range(1, m):
         answer = max(answer, dp[x][y])
+
+
 
 print(answer)
