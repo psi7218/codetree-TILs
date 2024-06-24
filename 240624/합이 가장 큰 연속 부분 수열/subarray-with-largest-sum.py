4e1765872be1723200e3c691ca30334835b2a479
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-24 17:14:19 +0900

    Title: 합이 가장 큰 연속 부분 수열, Time: 194ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240624/합이 가장 큰 연속 부분 수열/subarray-with-largest-sum.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [-int(1e9)] * n
+
+dp[0] = arr[0]
+
+for i in range(1,n):
+    dp[i] = max(dp[i-1] + arr[i], arr[i])
+
+# print(dp)
+print(max(dp))
