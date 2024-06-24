7f3b08185bc51da2c4a5b5401efc179d7c516562
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-24 10:40:03 +0900

    Title: 2차원 최대 증가 수열, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240624/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240624/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,9 +1,14 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[1] * m for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
 dp[0][0] = 1
 
+for k in range(1, n):
+    arr[k][0] = int(1e9)
+
+for l in range(1, m):
+    arr[0][l] = int(1e9)
 for i in range(1, n):
     for j in range(1, m):
        for s in range(i):
