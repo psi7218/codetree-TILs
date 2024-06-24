f23860897636f12d994520fa85303f465db89cc6
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-24 10:10:18 +0900

    Title: 최대 증가 부분 수열, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240624/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0] * n
+dp[0] = 1
+
+for i in range(1, n):
+    for j in range(0, i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
