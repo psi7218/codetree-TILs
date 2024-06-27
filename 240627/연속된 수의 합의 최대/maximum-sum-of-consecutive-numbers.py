e5b3feb5582a189f4a7a1433a7fb457d6914e51e
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-27 15:44:20 +0900

    Title: 연속된 수의 합의 최대, Time: 498ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240627/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.py
+++ 240627/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.py
@@ -3,22 +3,23 @@ a = [0] + list(map(int, input().split()))
 
 INF = int(1e9)
 
-dp = [[-1001 for _ in range(n + 1)] for _ in range(n + 1)]
+dp = [[-1001 for _ in range(k + 2)] for _ in range(n + 1)]
 answer = -INF
 
 for i in range(1, n + 1):
-    for j in range(i + 1):
+    for j in range(1,k + 2):
         if j == 1:
             dp[i][j] = a[i]
 
+        elif j >= k:
+            dp[i][j] = max(dp[i - 1][j - 1] + a[i], dp[i-1][j] + a[i])
         else:
-            dp[i][j] = dp[i-1][j-1] + a[i]
-# 
+            dp[i][j] = dp[i - 1][j - 1] + a[i]
 # for line in dp:
 #     print(line)
 
 for x in range(1, n+1):
-    for y in range(k,n+1):
+    for y in range(k,k+2):
         answer = max(answer, dp[x][y])
 
 print(answer)
