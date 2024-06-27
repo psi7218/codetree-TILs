6a7bef22442c965e0668e8b33c1f03691fcef4cf
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-28 08:31:20 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240628/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,26 @@
+java.io.*;
+java.util.*;
+
+public class Main {
+    static int n, m;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+
+        for (int i=0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        
+
+    }
+}
