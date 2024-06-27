50b4b2251037104b4471c1c6c7d6b8dec5d49d3c
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-27 15:18:18 +0900

    Title: 연속된 수의 합의 최대, Time: 493ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240627/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+a = [0] + list(map(int, input().split()))
+
+INF = int(1e9)
+
+dp = [[-INF for _ in range(n + 1)] for _ in range(n + 1)]
+answer = -INF
+
+for i in range(1, n + 1):
+    for j in range(i + 1):
+        if j == 1:
+            dp[i][j] = a[i]
+
+        else:
+            dp[i][j] = dp[i-1][j-1] + a[i]
+# 
+# for line in dp:
+#     print(line)
+
+for x in range(1, n+1):
+    for y in range(k,n+1):
+        answer = max(answer, dp[x][y])
+
+print(answer)
