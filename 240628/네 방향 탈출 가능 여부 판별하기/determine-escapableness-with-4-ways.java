ae97d9bb0f078af8c60b838419313a7954e6c1a2
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-06-28 18:32:54 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 129ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240628/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 240628/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,11 +1,14 @@
-java.io.*;
-java.util.*;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int n, m;
     static int[][] arr;
     static boolean[][] visited;
-    public static void main(String[] args) {
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+
+    public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,12 +26,36 @@ public class Main {
             }
         }
 
-
-
+        System.out.println(bfs());
     }
 
-    public static void bfs() {
+    public static int bfs() {
         Deque<int[]> q = new ArrayDeque<>();
+
+        q.offer(new int[]{0,0});
+        visited[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+
+            if (x == n-1 && y == n-1) {
+                return 1;
+            }
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (arr[nx][ny] == 1 && visited[nx][ny] == false) {
+                        q.offer(new int[]{nx,ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        return 0;
     }
 
 }
