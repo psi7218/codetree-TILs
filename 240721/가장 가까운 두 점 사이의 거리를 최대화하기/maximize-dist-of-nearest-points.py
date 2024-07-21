e4fc3e38b7f695e0e3f8574139139a1f8a1b44a0
psi7218 <133967948+psi7218@users.noreply.github.com>
2024-07-21 16:02:46 +0900

    Title: 가장 가까운 두 점 사이의 거리를 최대화하기, Time: 798ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240721/가장 가까운 두 점 사이의 거리를 최대화하기/maximize-dist-of-nearest-points.py
@@ -0,0 +1,31 @@
+n = int(input())
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+lst.sort()
+
+left = 1
+right = 10 ** 9
+answer = 0
+
+
+def func(val):
+    cnt = 0
+    temp = -10**9
+    for a, b in lst:
+        temp = max(a, temp + val)
+        if a <= temp <= b:
+            cnt += 1
+
+    return cnt >= n
+
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if func(mid):
+        answer = max(answer, left)
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(answer)
